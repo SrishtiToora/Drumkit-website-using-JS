998fe166dcae4068cbee01bd851745242b975887
SrishtiToora <77147148+SrishtiToora@users.noreply.github.com>
2021-05-29 23:14:02 +0530

    drums website complete

---
+++ index.js
@@ -0,0 +1,50 @@
+for (var i = 0; i < document.querySelectorAll(".but").length; i += 1) {
+  document.querySelectorAll(".but")[i].addEventListener("click", function() {
+    makeSound(this.innerHTML);
+    animate(this.innerHTML);
+  });
+}
+document.addEventListener("keypress",function(event){
+  makeSound(event.key);
+  animate(event.key);
+});
+
+function makeSound(key){
+  switch (key) {
+    case "w":
+      var audio = new Audio("sounds/crash.mp3");
+      audio.play();
+      break;
+    case "a":
+      var audio = new Audio("sounds/kick-bass.mp3");
+      audio.play();
+      break;
+    case "s":
+      var audio = new Audio("sounds/tom-1.mp3");
+      audio.play();
+      break;
+    case "d":
+      var audio = new Audio("sounds/tom-2.mp3");
+      audio.play();
+      break;
+    case "j":
+      var audio = new Audio("sounds/tom-3.mp3");
+      audio.play();
+      break;
+    case "k":
+      var audio = new Audio("sounds/tom-4.mp3");
+      audio.play();
+      break;
+    case "l":
+      var audio = new Audio("sounds/snare.mp3");
+      audio.play();
+      break;
+    default:console.log(val);
+  }
+}
+
+function animate(key){
+  var activeBut= document.querySelector("."+key);
+  activeBut.classList.add("pressed");
+  setTimeout(function(){activeBut.classList.remove("pressed");}, 500);
+}
